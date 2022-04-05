d003b85af9ab26e791e1fdc00ddc953f6c9901f0
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2022-04-05 22:40:21 +1000

    some improvement update

--- virsh.cluster.list-domain-if-addr.sh
+++ virsh.cluster.list-domain-if-addr.sh
@@ -2,6 +2,11 @@
 
 source ./virsh.cluster.info
 
+# ensure required variable are defined.
+set -u
+#: "${hosts}"
+: "${hosts:?variable 'hosts' is not set or empty.}"
+
 for name in ${hosts[@]}; do
   virsh domifaddr ${prefix}-${name}
 done
